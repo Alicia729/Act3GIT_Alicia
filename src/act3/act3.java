5753d808c5214d180ca9f6430fce0df50a351c01
Alicia <acurper729@g.educaand.es>
2024-11-25 17:59:42 +0100

    Añado mi primer commit

---
+++ src/act3/act3.java
@@ -0,0 +1,29 @@
+package act3;
+
+import java.util.Random;
+
+public class act3 {
+
+	public static void main(String[] args) {
+		Random random = new Random();
+		
+		
+		
+	
+		
+		int[] arrayAleatorio = new int[10];
+		 for (int i = 0; i < arrayAleatorio.length; i++) {
+	            arrayAleatorio[i] = random.nextInt(20) + 1; // Genera un número entre 1 y 100
+	        }
+		 for (int num : arrayAleatorio) {
+	            System.out.print(num + " ");
+	        }
+
+	}
+	public static int generarAleatorio() {
+		Random random = new Random();
+		int aleatorio = random.nextInt();
+		return aleatorio;
+	}
+
+}
