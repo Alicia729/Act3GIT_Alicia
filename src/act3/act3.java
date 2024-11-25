fc04cf4779ccbac1f1bf47a08af7ec202ff817ea
Alicia <acurper729@g.educaand.es>
2024-11-25 19:30:05 +0100

    He modificado el proyecto

--- src/act3/act3.java
+++ src/act3/act3.java
@@ -8,7 +8,7 @@ public class act3 {
 	public static void main(String[] args) {
 		Random random = new Random();
 		Scanner scanner = new Scanner (System.in);
-		int num1, num2;
+		int num1, num2, num3;
 		
 	    while (true) {
             System.out.print("Introduce el primer número entero: ");
@@ -16,39 +16,23 @@ public class act3 {
 
             System.out.print("Introduce el segundo número entero: ");
             num2 = scanner.nextInt();
+            
+            System.out.print("Introduce el tercer número entero: ");
+            num3 = scanner.nextInt();
 
-            if (num1 != num2) {
-            	 System.out.println("Los números deben ser distintos. Inténtalo de nuevo.");
+
+            // Verificar si el tercer número está entre los otros dos
+            if (num3 > Math.min(num1, num2) && num3 < Math.max(num1, num2)) {
+                System.out.println("El número " + num3 + " está entre " + num1 + " y " + num2);
+            } else {
+                System.out.println("El número " + num3 + " no está entre " + num1 + " y " + num2);
             }
-            int menor = Math.min(num1, num2);
-            int mayor = Math.max(num1, num2);
-
-            System.out.println("\nNúmeros entre " + menor + " y " + mayor + ", incrementando de 7 en 7:");
-
-            for (int i = menor; i < mayor; i += 7) {
-                System.out.println(i);
-            }
-
-            scanner.close();
+        }
 		
 		
 		
 		
 		
-		
-		int[] arrayAleatorio = new int[10];
-		 for (int i = 0; i < arrayAleatorio.length; i++) {
-	            arrayAleatorio[i] = random.nextInt(20) + 1; // Genera un número entre 1 y 20
-	        }
-		 for (int num : arrayAleatorio) {
-	            System.out.print(num + " ");
-	        }
-
-	}
-	public static int generarAleatorio() {
-		Random random = new Random();
-		int aleatorio = random.nextInt();
-		return aleatorio;
 	}
 
 }
