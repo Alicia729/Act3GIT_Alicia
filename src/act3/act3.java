a65b60ba10c6a493855b8b898673fb91aaedd9ff
Alicia <acurper729@g.educaand.es>
2024-11-25 19:23:34 +0100

    He añadido un nuevo programa

--- src/act3/act3.java
+++ src/act3/act3.java
@@ -1,11 +1,38 @@
 package act3;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class act3 {
 
 	public static void main(String[] args) {
 		Random random = new Random();
+		Scanner scanner = new Scanner (System.in);
+		int num1, num2;
+		
+	    while (true) {
+            System.out.print("Introduce el primer número entero: ");
+            num1 = scanner.nextInt();
+
+            System.out.print("Introduce el segundo número entero: ");
+            num2 = scanner.nextInt();
+
+            if (num1 != num2) {
+            	 System.out.println("Los números deben ser distintos. Inténtalo de nuevo.");
+            }
+            int menor = Math.min(num1, num2);
+            int mayor = Math.max(num1, num2);
+
+            System.out.println("\nNúmeros entre " + menor + " y " + mayor + ", incrementando de 7 en 7:");
+
+            for (int i = menor; i < mayor; i += 7) {
+                System.out.println(i);
+            }
+
+            scanner.close();
+		
+		
+		
 		
 		
 		
